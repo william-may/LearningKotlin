3c825e180af0cbe4e9303a23c1ca82fb053d2a52
William May <wmay@aip.com>
2023-07-08 16:06:53 -0700

    finished video

--- src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
+++ src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
@@ -1,5 +1,11 @@
 package com.virtualpairprogrammers.learningkotlin.kotlin
 
+import java.util.*
+
 fun main() {
-    System.out.println("Hello, World");
+    var name = "Bill"
+    val surname = "May"
+
+    println("Hello $name ${surname.uppercase(Locale.getDefault())}")
+    println("the \$name variable has ${name.length} characters")
 }
