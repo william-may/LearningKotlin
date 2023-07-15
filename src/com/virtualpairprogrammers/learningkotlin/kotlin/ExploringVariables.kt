bac7736c3e3ee059659b164e1b599ad96a58f3d4
William May <wmay@aip.com>
2023-07-15 11:55:38 -0700

    finished video

--- src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
+++ src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
@@ -1,9 +1,14 @@
 package com.virtualpairprogrammers.learningkotlin.kotlin
 
+import kotlin.math.roundToInt
+
 fun main() {
     val myDouble = 21.4
     println("Is myDouble a Double? ${myDouble is Double}")
-    println("myDboule is a ${myDouble::class.qualifiedName}")
+    println("myDouble is a ${myDouble::class.qualifiedName}")
 
     println("myDoubles java class is a ${myDouble.javaClass}")
+
+    val myInteger = myDouble.roundToInt()
+    println("myInteger is a ${myInteger::class.qualifiedName}")
 }
