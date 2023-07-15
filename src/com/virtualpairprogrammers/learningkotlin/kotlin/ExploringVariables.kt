a2074dac4042e7b475197060c32f8e8fc9aeebac
William May <wmay@aip.com>
2023-07-15 12:12:16 -0700

    getting ready to push it real good

--- src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
+++ src/com/virtualpairprogrammers/learningkotlin/kotlin/ExploringVariables.kt
@@ -1,5 +1,29 @@
 package com.virtualpairprogrammers.learningkotlin.kotlin
 
-fun main() {
+import java.math.BigDecimal
+import kotlin.math.roundToInt
 
+fun main() {
+    val myDouble = 21.4
+    println("Is myDouble a Double? ${myDouble is Double}")
+    println("myDouble is a ${myDouble::class.qualifiedName}")
+
+    println("myDoubles java class is a ${myDouble.javaClass}")
+
+    val myInteger = myDouble.roundToInt()
+    println("myInteger is a ${myInteger::class.qualifiedName}")
+
+    val anotherInteger : Int = 17
+
+    val myFloat : Float = 13.6f
+    val myLong : Long = 1L;
+
+    val result = myFloat + anotherInteger;
+    println("The result is $result")
+
+    val bd : BigDecimal = BigDecimal(17)
+    println("bd is $bd")
+
+    val bd2 : BigDecimal = bd.add(BigDecimal(30))
+    println("bd2 is $bd2")
 }
