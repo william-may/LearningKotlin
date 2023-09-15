56f0a35297b602c3874521f4b439400f0b86ac88
William May <wmay@aip.com>
2023-09-14 19:02:43 -0700

    done

--- src/com/virtualpairprogrammers/learningkotlin/ExploringLoops.kt
+++ src/com/virtualpairprogrammers/learningkotlin/ExploringLoops.kt
@@ -4,7 +4,12 @@ package com.virtualpairprogrammers.learningkotlin
 fun main(args: Array<String>) {
     whileLoop()
     forLoop()
+    destructuredForLoop()
+    iterateOverMap()
+    iterateWithRange()
 }
+
+
 
 fun whileLoop() {
     println("while Loop")
@@ -26,4 +31,40 @@ fun forLoop() {
 
 fun destructuredForLoop() {
 
+    val people = listOf(
+        Person(1, "Mr", "James", "Apple", null),
+        Person(2, "Miss", "Sophie", "Orange", null),
+        Person(3, "Mrs", "Anita", "Cherry", null)
+    )
+
+    for((id, title, surName) in people) {
+        println("$title $surName has id $id")
+    }
+
+}
+
+fun iterateOverMap() {
+    val people = HashMap<Int, Person>()
+
+    people[1] = Person(1, "Mr", "James", "Apple", null)
+    people[2] = Person(2, "Miss", "Sophie", "Orange", null)
+    people[3] = Person(3, "Mrs", "Anita", "Cherry", null);
+
+    for((key, value) in people) {
+        println("${value.title} ${value.lastName} has id ${value.id}")
+    }
+}
+
+fun iterateWithRange() {
+    val myRange = 0..3
+
+    for(i in myRange) {
+        println(i)
+    }
+
+    println("As a lambda")
+    myRange.forEach { println(it) }
+
+    println("Count backwards")
+    (9 downTo 0).forEach { println(it) }
 }
